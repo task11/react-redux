5ac7c8990f8bc8e43bc74505a987f9be4ec498b9
task11 <task11@naver.com>
2022-01-02 16:40:10 +0900

    [Refator] VanillaJS Redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const countModifier = (count = 0, action) => { // Reducer!!!!!
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,11 +27,11 @@ const onChange = () => {
 countStore.subscribe(onChange)
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD }); // action is object
 }
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 }
 
 add.addEventListener("click", handleAdd);
