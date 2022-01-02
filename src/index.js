8ebefbd706d5cfcf980ad731b06af6eb33519bd3
task11 <task11@naver.com>
2022-01-02 16:34:24 +0900

    [Feat] VanillaJS Redux.proto

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+
+
+const countModifier = (count = 0, action) => { // Reducer!!!!!
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange)
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+}
+
+add.addEventListener("click", handleAdd);
+
+minus.addEventListener("click", handleMinus);
