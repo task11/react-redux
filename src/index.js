25ccc31c230f4b6e1136c242be66cfbf55e0aac0
task11 <task11@naver.com>
2022-06-22 10:42:39 +0900

    feat: init ex project & without redux

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
-import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+import App from './App';
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }]; // ...state -> ES6 Spread
-    case DELETE_TODO:
-      return [];
-    default:
-      return state;
-  }
-
-}
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()))
-
-const onSubmit = (event) => {
-  event.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
-};
-
-
-
-
-form.addEventListener("submit", onSubmit);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
